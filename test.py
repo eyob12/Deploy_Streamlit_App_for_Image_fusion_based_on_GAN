eebe15b1d4c00777939edaf91b5f863f83d3e32a
Eyob Abera Deboch <57870274+eyob12@users.noreply.github.com>
2023-05-12 20:59:04 +0800

    test.py

--- test.py
+++ test.py
@@ -420,8 +420,8 @@ path = '_100_onlyadd_THREE22'
 #reader = tf.train.load_checkpoint('./checkpoint_20/ENH_CGAN'+path+'/CGAN.model-'+ str(num_epoch))
 
 checkpoint_path = './checkpoint_20/ENH_CGAN'+path+'/CGAN.model-'+ str(num_epoch)
-checkpoint = tf.train.load_checkpoint(checkpoint_path)
-var_names = checkpoint.get_variable_to_shape_map().keys()
+reader = tf.train.load_checkpoint(checkpoint_path)
+var_names = reader.get_variable_to_shape_map().keys()
 # retrieve the variable values
 var_values = [checkpoint.get_tensor(var_name) for var_name in var_names]
 
